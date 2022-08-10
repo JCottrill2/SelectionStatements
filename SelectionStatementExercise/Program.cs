31b1efd4c312c574cb67056e19658e609ac4f21b
jess <jcottrill1123@gmail.com>
2022-08-10 15:39:16 -0400

    initial commit

---
+++ SelectionStatementExercise/Program.cs
@@ -0,0 +1,46 @@
+﻿//var r = new Random();
+//var favNumber = r.Next(1, 100);
+//Console.WriteLine("Can you guess my favorite number?");
+//var userInput = int.Parse(Console.ReadLine());
+
+//if (userInput < favNumber)
+//{
+//    Console.WriteLine("The variable is too low");
+//}
+
+//else if (userInput > favNumber)
+//{
+//    Console.WriteLine("The variable is too high");
+//}
+
+//else
+//{
+//    Console.WriteLine("Good Job! You got it right!");
+//}
+
+Console.WriteLine("What is your favorite subject in school?");
+string userInput = Console.ReadLine();
+
+switch (userInput.ToLower())
+{
+    case "science":
+        Console.WriteLine("Mine too!");
+        break;
+    case "math":
+        Console.WriteLine("Do you know Pi?");
+        break;
+    case "english":
+        Console.WriteLine("To be or not to be?");
+        break;
+    case "history":
+        Console.WriteLine("Do you know Alexander Hamilton?");
+        break;
+    case "lunch":
+        Console.WriteLine("Food is good!");
+        break;
+        default: Console.WriteLine("Oh wow! I didn't think of that!");
+        break;
+}
+    
+
+    
